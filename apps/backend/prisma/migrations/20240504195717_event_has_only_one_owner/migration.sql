3aac7cdcfd4ba5411298b06b96d74a52776923f9
Alex Csiszár <csiszaralex@gmail.com>
2024-05-04 22:21:08 +0200

    Adds migrations

---
+++ apps/backend/prisma/migrations/20240504195717_event_has_only_one_owner/migration.sql
@@ -0,0 +1,19 @@
+-- DropForeignKey
+ALTER TABLE "Event" DROP CONSTRAINT "Event_ownerGroupId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Event" DROP CONSTRAINT "Event_ownerUserId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_ownerUserId_fkey" FOREIGN KEY ("ownerUserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_ownerGroupId_fkey" FOREIGN KEY ("ownerGroupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- The Event table should have only one owner, either a user or a group. The ownerUserId and ownerGroupId columns should be mutually exclusive. If an event has an ownerUserId, it should not have an ownerGroupId, and vice versa.
+ALTER TABLE IF EXISTS  "Event"
+  ADD CONSTRAINT only_one_owner
+  CHECK (
+    ("ownerUserId" IS NOT NULL AND "ownerGroupId" IS NULL) OR
+    ("ownerUserId" IS NULL AND "ownerGroupId" IS NOT NULL)
+  );
