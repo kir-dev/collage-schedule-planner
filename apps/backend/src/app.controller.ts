469392a4020c6964bd92b7aa232543536422587a
Anujkumar Darji <anujdarji100737@gmail.com>
2024-05-06 13:20:21 +0400

    Basic Health Check API Endpoint closes #4

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -10,4 +10,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/health')
+  getHealth(): { message: string } {
+    return { message: 'Server is running' };
+  }
 }
