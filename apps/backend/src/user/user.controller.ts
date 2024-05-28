e260114f95dfd96b73f047c6092b099850a124ad
GodriSzabolcs <godri04@gmail.com>
2024-05-28 19:11:19 +0200

    Error handling

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
