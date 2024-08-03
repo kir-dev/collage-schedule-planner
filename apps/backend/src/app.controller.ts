cf10c594a39d5456151d21f433e67e215c0fca29
Bálint Király <balintkiraly.dev@gmail.com>
2024-08-03 15:18:53 +0200

    Fix requested changes on health check api

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { AppService } from './app.service';
+
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
   @Get('/health')
   getHealth(): string {
-    return 'Service is up and running';
+    return this.appService.getHealth();
   }
 }
--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [PrismaModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
